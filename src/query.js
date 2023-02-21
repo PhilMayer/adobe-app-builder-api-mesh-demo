2c54a1b9846460510d38908e39bd56b35e07abd9
Phil Mayer <pmayer@adobe.com>
2023-02-21 14:52:35 -0500

    Add banner

--- src/query.js
+++ src/query.js
@@ -1,36 +1,9 @@
-const SEARCH_TERM = "earings";
+const SEARCH_TERM = "earrings";
 const NUMBER_OF_RESULTS = 6;
-
-// const query = {
-//   query: `{
-//       products(search: "${SEARCH_TERM}", pageSize: ${NUMBER_OF_RESULTS}) {
-//         total_count
-//         items {
-//           name
-//           sku
-//           description {
-//             html
-//           }
-//           price_range {
-//             minimum_price {
-//               regular_price {
-//                 value
-//                 currency
-//               }
-//             }
-//           }
-//           image {
-//             url
-//           }
-//         }
-//       }
-
-//     }`
-// };
 
 const query = {
   query: `{
-    products(search: "earings") {
+    products(search: "${SEARCH_TERM}") {
       items {
         name
         sku
