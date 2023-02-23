2ee82fa613b3dc28b58c03e67170738fb07422f2
Phil Mayer <pmayer@adobe.com>
2023-02-23 16:27:29 -0500

    Add top nav

--- src/App.js
+++ src/App.js
@@ -44,10 +44,10 @@ class APIMeshExample extends React.Component {
     return (
       <>
         <div>
-          <h2 className="header">Your API Mesh query results 
-            <p className="sub-header"><a href="https://adobe.enterprise.slack.com/files/T025FJ55H/F04G1VALPH9">Learn how to set up App Builder and API Mesh</a></p>
-          </h2>
-          
+          <p className="sub-header"><a href="https://adobe.enterprise.slack.com/files/T025FJ55H/F04G1VALPH9">&#128214;  How to set up App Builder and API Mesh</a></p>
+          <img className="nav" src="nav.png"/>
+          <h2>Your API Mesh query results</h2>
+            
           
           <ul>
             {this.state.products.map((item, idx) => (
