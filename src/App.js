320403fa796ec76465276dafa1f931d5f8e1ae99
Phil Mayer <pmayer@adobe.com>
2024-01-23 12:45:31 -0500

    Remove iframe

--- src/App.js
+++ src/App.js
@@ -156,13 +156,6 @@ class APIMeshExample extends React.Component {
               <CodeSidebar meshResponse={this.state.apiMeshRes} />
             </div>
           </div>
-          <iframe
-            src="https://codesandbox.io/p/github/PhilMayer/adobe-app-builder-api-mesh-demo/bodea?file=%2Fmesh.json&embed=1"
-            style={{width:'100%', height: '800px', border:0, borderRadius: '4px', overflow:'hidden'}}
-            title="PhilMayer/adobe-app-builder-api-mesh-demo/bodea"
-            allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-            sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-          ></iframe>
         </div>
       </>
     );
